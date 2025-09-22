706014ed2ac498bfc5f1b249634f892244b97d1a
Anirvinya <anirvinya.kolli.01@gmail.com>
2025-09-22 09:09:07 -0400

    Add files via upload

---
+++ AgeProgram.cpp
@@ -0,0 +1,50 @@
+// Sai Anirvinya Kolli 
+// 9/22/2025
+// Ifstatement lab 
+
+#include <iostream>
+
+
+
+int main()
+{
+    //Arrays
+    std::string minorThings[3] = {"have to go to school", "have backpacks", "play with legos"};
+    std::string adultThings[4] ={"pay taxes", "have kids", "get married", "work at a job"};
+    std::string seniorThings[3] ={"retire", "relax", "go to a retiremnt home"};
+
+    //input 
+    int age; 
+    std:: string country;
+    std::cout << "HEYA; Enter your age"; 
+    std::cin >> age; 
+    std::cout << "HEYA; Enter your country"; 
+    std::cin >> country; 
+
+    //if statement 
+    if (age  < 18) {
+        std::cout << "Things you can do as a minor in " << country;
+        for (int i = 0; i < 3; i++){
+            std::cout << minorThings[i] << ", ";
+        }
+        std::cout << "\n";
+    }
+    else if (age < 64) {
+        std::cout << "Things you can do as a adult in " << country;
+        for (int i = 0; i < 4; i++){
+            std::cout << adultThings[i]<< ", ";
+        }
+        std::cout << "\n";
+    }
+    else {
+        std::cout << "Things you can do as a senior in " << country;
+        for (int i = 0; i < 3; i++){
+            std::cout << seniorThings[i] << ", ";
+        }
+        std::cout << "\n";
+    }
+return 0;
+}
+
+
+
